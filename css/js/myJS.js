0b215ac8757af1a01e3cd7d63523dc1f0c189a3c
deegardner8 <deegardner8@byui.edu>
2020-04-25 10:40:40 -0600

    SP2020

--- css/js/myJS.js
+++
@@ -1,2 +0,0 @@
-const options = {weekday: 'long', day: 'numberic', month: 'long', year: 'numeric'};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', optoins);
---
+++ js/myJS.js
@@ -0,0 +1,10 @@
+
+//The following of Code if from w3schools https://www.w3schools.com/js/js_date_methods.asp
+var d = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+document.getElementById('replace-time').innerHTML =  days[d.getDay()] + " " + months[d.getMonth()] + " " + d.getDay() + " " + d.getFullYear();
+document.getElementById('replace-year').innerHTML =  d.getFullYear()-3;
